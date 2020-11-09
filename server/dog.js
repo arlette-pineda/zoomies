cfd3808dbfccc02afa5607afd8b6793a0540683e
Arlette Pineda <arlettepimo@gmail.com>
2020-11-09 22:28:54 +0000

    Created server/public file and added index.js file.

---
+++ server/dog.js
@@ -0,0 +1,18 @@
+
+class Dog {
+  constructor(id, name, breed, age, size, description, location, gender, coat, color, traits, photos){
+    this.id = id;
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+    this.size = size;
+    this.description = description;
+    this.location = location;
+    this.gender = gender;
+    this.coat = coat;
+    this.color = color;
+    this.traits = traits;
+    this.photos = photos;
+  }
+}
+module.exports = Dog;
---
+++ server/photos.js
@@ -0,0 +1,9 @@
+class Photos{
+  constructor(small, medium, large, full) {
+    this.small = small;
+    this.medium = medium;
+    this.large = large;
+    this.full = full;
+  }
+}
+module.exports = Photos;
