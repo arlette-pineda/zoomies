4ba33e76c4b18e9213d043bacc1422d6aea7393f
Arlette Pineda <arlettepimo@gmail.com>
2020-11-11 17:25:35 +0000

    Renamed new petfinderService variable and cleaned up commented out code in index.js.

--- server/index.js
+++ server/index.js
@@ -3,16 +3,10 @@ const app = express();
 const petfinderService = require('./petfinder-service');
 const port = 3000;
 
-// app.get('/animals/:animalId', function (req, res) {
-//   res.send('hello world')
-// })
-
 app.get('/animals/:animalId', async (req, res) => {
   try {
-    let pet = petfinderService;
-    let a = new pet();
-    // console.log('req.params', req.params);
-    let animalResult = await a.getAnimal(req.params.animalId);
+    let pfService = new petfinderService();
+    let animalResult = await pfService.getAnimal(req.params.animalId);
     return res.json(animalResult);
   } catch (error) {
     console.log(error)
