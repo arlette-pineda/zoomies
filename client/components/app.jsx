657a7e72a92612303b39c9e01bd5c3eb68de7f1a
Arlette Pineda <arlettepimo@gmail.com>
2020-11-19 21:18:19 +0000

    Footer styling.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,23 +3,9 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Test from './test-page';
 import Second from './second-test';
 import NavBar from './nav-bar';
+import Footer from './footer';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: null,
-      isLoading: true
-    };
-  }
-
-  componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
-  }
 
   render() {
     return (
@@ -28,6 +14,7 @@ export default class App extends React.Component {
         <div>
           <p>We&apos;re adding Routing!</p>
         </div>
+        <Footer/>
         <Route path="/" exact component={Test}/>
         <Route path="/second" component={Second} />
 
---
+++ client/components/footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  text: {
+    padding: theme.spacing(2, 10, 0),
+    color: 'black'
+  },
+  appBar: {
+    top: 'auto',
+    bottom: 0,
+    backgroundColor: '#FEC600'
+  }
+}));
+
+export default function Footer() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography className={classes.text} variant="h5" gutterBottom>
+            &copy; 2020 Zoomies
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+}
