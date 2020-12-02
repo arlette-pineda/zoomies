7abb83eb5ae138ffd3fe3e5ada0c71487c7ab14c
Arlette Pineda <arlettepimo@gmail.com>
2020-12-02 22:21:14 +0000

    Created ages array.

--- client/components/search-form.jsx
+++ client/components/search-form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
@@ -49,23 +49,47 @@ const top100Films = [
   { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 }
 ];
 
-export default function SearchForm() {
+const ages = [
+  'baby',
+  'young',
+  'adult',
+  'senior'
+];
+
+export default function SearchForm(props) {
   const classes = useStyles();
+  // const [breed, setBreed] = useState('');
+  // const [age, setAge] = useState('');
+  // const [size, setSize] = useState('');
+  const [breedList, setBreedList] = useState([]);
+  const [hasError, setErrors] = useState(false);
+
+  useEffect(() => {
+    fetch('/api/dogBreeds')
+      .then(response => response.json())
+      .then(res => setBreedList(res))
+      .catch(() => setErrors(true));
+  }, []);
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    alert(`Submitting Name ${name}`);
+  };
 
   return (
-    <form className={classes.root} noValidate>
-      <InputLabel className={classes.labelStyle} id="demo-simple-select-placeholder-label-label">
+    <form className={classes.root} noValidate onSubmit={handleSubmit}>
+      <InputLabel className={classes.labelStyle} id="input-label-breed">
         Breed
       </InputLabel>
       <Autocomplete
         id="combo-box-demo"
-        options={top100Films}
-        getOptionLabel={option => option.title}
+        options={breedList}
+        getOptionLabel={option => option}
         className="search-input-styling"
         renderInput={params => <TextField {...params} variant="outlined" />}
       />
-      <InputLabel className={classes.labelStyle} id="demo-simple-select-placeholder-label-label">
-        Age
+      <InputLabel className={classes.labelStyle} id="input-label-age">
+    Age
       </InputLabel>
       <Autocomplete
         id="combo-box-demo"
@@ -74,14 +98,15 @@ export default function SearchForm() {
         className="search-input-styling"
         renderInput={params => <TextField {...params} variant="outlined" />}
       />
-      <InputLabel className={classes.labelStyle} id="demo-simple-select-placeholder-label-label">
-        Size
+      <InputLabel className={classes.labelStyle} id="input-label-size">
+    Size
       </InputLabel>
       <Autocomplete
         id="combo-box-demo"
         options={top100Films}
         getOptionLabel={option => option.title}
         className="search-input-styling"
+        // onChange={e => setSize(e.target.value)}
         renderInput={params => <TextField {...params} variant="outlined" />}
       />
       <div className={classes.buttonsDiv}>
