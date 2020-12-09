1784192fa7fff073f37867e3bfcb7ec1b90560db
Arlette Pineda <arlettepimo@gmail.com>
2020-12-09 01:03:11 +0000

    About us structure.

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 export default function About() {
 
   return (
-    <div>Hi this is the ABOUT page!!</div>
+    <div>Hi this is the ABOUT page!!
+
+      ABOUT US
+
+    <p>Info about us goes here.</p>
+    <button>Search &gt;&gt;</button>
+    </div>
   );
 }
