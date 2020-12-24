ee4af6294bc50ac4f06b4b82d2bee41fb0449e03
Arlette Pineda <arlettepimo@gmail.com>
2020-12-24 00:59:18 +0000

    Added margin to chips for spacing.

--- client/components/chips.jsx
+++ client/components/chips.jsx
@@ -6,6 +6,9 @@ import Avatar from '@material-ui/core/Avatar';
 const useStyles = makeStyles(theme => ({
   theMargin: {
     marginLeft: theme.spacing(2)
+  },
+  chipMargin: {
+    marginRight: theme.spacing(0.5)
   }
 }));
 
@@ -29,26 +32,29 @@ export default function Chips(props) {
       {props.breed
         ? <Chip
           size="small"
-          avatar={<Avatar alt="Natacha" src="/images/zoomies-icon.png" />}
+          avatar={<Avatar alt="Zoomies icon" src="/images/zoomies-icon.png" />}
           label={props.breed}
           onDelete={handleDeleteBreed}
+          className={classes.chipMargin}
         />
         : null
       }
       {props.age
         ? <Chip
           size="small"
-          avatar={<Avatar alt="Natacha" src="/images/zoomies-icon.png" />}
+          avatar={<Avatar alt="Zoomies icon" src="/images/zoomies-icon.png" />}
           label={props.age}
           onDelete={handleDeleteAge}
+          className={classes.chipMargin}
         />
         : null}
       {props.size
         ? <Chip
           size="small"
-          avatar={<Avatar alt="Natacha" src="/images/zoomies-icon.png" />}
+          avatar={<Avatar alt="Zoomies icon" src="/images/zoomies-icon.png" />}
           label={props.size}
           onDelete={handleDeleteSize}
+          className={classes.chipMargin}
         />
         : null
       }
