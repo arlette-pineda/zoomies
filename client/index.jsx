bd8ae444b2bba5ddd7146ddd39c71e9a6feba361
Arlette Pineda <arlettepimo@gmail.com>
2020-12-29 00:56:23 +0000

    Theme added for font.

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import App from './components/app';
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Bubbler One, Helvetica, Arial, sans-serif',
+    fontWeight: 'bold'
+  }
+});
+
 ReactDOM.render(
-  <App />,
+  <MuiThemeProvider theme = { theme }>
+    <App />
+  </MuiThemeProvider>,
   document.querySelector('#root')
 );
