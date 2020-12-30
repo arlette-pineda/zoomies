eb793d79ba912ec4e077a3a59276c9bdfdf2571f
Arlette Pineda <arlettepimo@gmail.com>
2020-12-30 21:25:04 +0000

    Redid menu for larger screens to display links on header bar.

--- client/components/nav-bar.jsx
+++ client/components/nav-bar.jsx
@@ -8,6 +8,8 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Hidden from '@material-ui/core/Hidden';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,6 +26,13 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     color: 'black',
     width: '50%'
+  },
+  menuButtonsLg: {
+    fontWeight: 'bold',
+    fontSize: '2rem',
+    letterSpacing: '1px',
+    color: 'black',
+    margin: '0 9px'
   }
 }));
 
@@ -47,22 +56,29 @@ function NavBar() {
           <Typography component={Link} to='/' variant="h6" className={classes.title}>
             <img src="/images/zoomies-img.png" alt="zoomies logo" className="logo-img"/>
           </Typography>
-          <IconButton edge="start" className={classes.menuButton} aria-label="simple-menu" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-            <MenuIcon/>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-              getContentAnchorEl={null}
-              anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-            >
-              <MenuItem component={Link} to='/'>Home</MenuItem>
-              <MenuItem component={Link} to='/about'>About</MenuItem>
-              <MenuItem component={Link} to='/search'>Search</MenuItem>
-            </Menu>
-          </IconButton>
+          <Hidden lgUp>
+            <IconButton edge="start" className={classes.menuButton} aria-label="simple-menu" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+              <MenuIcon/>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                getContentAnchorEl={null}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+              >
+                <MenuItem component={Link} to='/'>Home</MenuItem>
+                <MenuItem component={Link} to='/about'>About</MenuItem>
+                <MenuItem component={Link} to='/search'>Search</MenuItem>
+              </Menu>
+            </IconButton>
+          </Hidden>
+          <Hidden mdDown>
+            <Button className={classes.menuButtonsLg} component={Link} to='/' color="inherit">Home</Button>
+            <Button className={classes.menuButtonsLg} component={Link} to='/about' color="inherit">About</Button>
+            <Button className={classes.menuButtonsLg} component={Link} to='/search' color="inherit">Search</Button>
+          </Hidden>
         </Toolbar>
       </AppBar>
     </div>
