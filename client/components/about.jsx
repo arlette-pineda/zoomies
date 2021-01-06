c2b8b9397c305fb883e5b38fb3ee08c0e5f078a6
Arlette Pineda <arlettepimo@gmail.com>
2021-01-06 02:00:26 +0000

    P styling and page spacing.

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -13,9 +13,14 @@ const useStyles = makeStyles(theme => ({
     margin: '0 20%',
     fontWeight: '800'
   },
+  aboutUsHeader: {
+    letterSpacing: '2px'
+  },
   searchButtonStyle: {
     borderRadius: '10px',
-    backgroundColor: theme.palette.secondary.main
+    backgroundColor: theme.palette.secondary.main,
+    marginBottom: '5%',
+    fontSize: 'large'
   },
   iconStyling: {
     paddingLeft: '8px'
@@ -27,15 +32,15 @@ export default function About() {
 
   return (
     <div className={classes.root}>
-      <h1>About Us</h1>
-      <p>Hi there! Zoomies realizes that we are all going through difficult times right now
+      <h1 className={classes.aboutUsHeader}>About Us</h1>
+      <p className="about-us-p-lg">Hi there! Zoomies realizes that we are all going through difficult times right now
         and are missing some connection. We’d like to aid in that and why not do it with
         four-legged friends that are sure to leave a smile on anyone’s face!</p>
 
-      <p>Now you don’t have to worry whether far, allergic, or scared, we have a
+      <p className="about-us-p-lg">Now you don’t have to worry whether far, allergic, or scared, we have a
         perfect little (or big!) buddy for everybody! </p>
 
-      <p>Volunteers at our partner organizations are ready to assist and our good boys
+      <p className="about-us-p-lg">Volunteers at our partner organizations are ready to assist and our good boys
         and girls are eager to meet you!</p>
       <Button variant="contained" component={Link} to='/search' className={classes.searchButtonStyle}>
         Search <NavigateNextRoundedIcon className={classes.iconStyling} fontSize="large" />
