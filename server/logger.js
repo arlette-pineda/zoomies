83f521f42c869c242330e73c2e1e82657f18102f
Arlette Pineda <arlettepimo@gmail.com>
2021-02-24 01:53:21 +0000

    Created file to make logger reusable.

---
+++ server/logger.js
@@ -0,0 +1,10 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'debug',
+  transports: [
+    new winston.transports.File({ filename: 'info.log' })
+  ]
+});
+
+module.exports = logger;
