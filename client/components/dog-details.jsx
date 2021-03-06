1657b82304e7ae65698eee60546726cc97d49197
Arlette Pineda <arlettepimo@gmail.com>
2021-03-06 00:11:21 +0000

    Base CSS for mobile details view.

--- client/components/dog-details.jsx
+++ client/components/dog-details.jsx
@@ -5,37 +5,44 @@ import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    margin: theme.spacing(3),
-    backgroundColor: theme.palette.primary.main
+    margin: theme.spacing(3)
   },
-  containerOne: {
+  photosSection: {
     backgroundColor: '#cfe8fc',
     height: '30vh'
   },
-  containerTwo: {
-    height: '15vh',
-    borderRadius: '40px',
-    paddingLeft: '20px',
-    backgroundColor: 'white'
+  titleSection: {
+    height: '12vh',
+    borderRadius: '25px',
+    marginBottom: '35px',
+    marginTop: '-55px',
+    filter: 'drop-shadow(0 2mm 1mm lightgray)'
   },
-  containerThree: {
+  detailsSection: {
     backgroundColor: '#ffcff5',
     height: '35vh',
-    borderRadius: '40px 40px 0 0',
-    paddingLeft: '20px'
+    borderRadius: '25px 25px 0 0',
+    paddingLeft: '30px'
   },
-  containerFour: {
+  aboutMeSection: {
     backgroundColor: '#ffe1cf',
     height: '30vh',
-    paddingLeft: '20px'
+    paddingLeft: '30px'
   },
-  testOne: {
+  nameSub: {
     backgroundColor: 'white',
-    borderRadius: '40px 0 0 40px'
+    borderRadius: '25px 0 0 25px',
+    paddingLeft: '30px',
+    textShadow: '0 6px 14px gray'
   },
-  testTwo: {
+  scheduleArea: {
     backgroundColor: theme.palette.secondary.main,
-    borderRadius: '0 40px 40px 0'
+    borderRadius: '0 25px 25px 0',
+    textAlign: 'center',
+    paddingTop: '10px'
+  },
+  detailItems: {
+    filter: 'drop-shadow(0 2mm 1mm gray)'
   }
 }));
 
@@ -43,33 +50,36 @@ export default function DogDetails(props) {
 
   const classes = useStyles();
   return (
-    <div className={classes.root} id="content-wrap">
-      <Grid container>
-        <Grid item xs={12} className={classes.containerOne}>Photos</Grid>
-        <Grid container item xs={12} className={classes.containerTwo}>
-          <Grid item xs={8} className={classes.testOne}>
-            <h2>Dog Name</h2>
-            <h3>Dog Breed</h3>
+    <div id="content-wrap">
+      <div className={classes.photosSection}>Photos</div>
+      <div className={classes.root} >
+        <Grid container >
+          {/* <Grid item xs={12} className={classes.photosSection}>Photos</Grid> */}
+          <Grid container item xs={12} className={classes.titleSection}>
+            <Grid item xs={8} className={classes.nameSub}>
+              <h2>Dog Name</h2>
+              <h3>Dog Breed</h3>
+            </Grid>
+            <Grid item xs={4} className={classes.scheduleArea}>
+              <h3>Schedule Meet!</h3>
+            </Grid>
           </Grid>
-          <Grid item xs={4} className={classes.testTwo}>
-            <h3>Schedule Meet!</h3>
+          <Grid item xs={12} className={classes.detailsSection}>
+            <h2>Details</h2>
+            <div className={classes.detailItems}>
+              <p>Size: </p>
+              <p>Gender: </p>
+              <p>Age: </p>
+              <p>Coat: </p>
+              <p>Color: </p>
+              <p>Location: </p>
+            </div>
+          </Grid>
+          <Grid item xs={12} className={classes.aboutMeSection}>
+            <h2>About Me</h2>
           </Grid>
         </Grid>
-        <Grid item xs={12} className={classes.containerThree}>
-          <h3>Details</h3>
-          <div>
-            <p>Size: </p>
-            <p>Gender: </p>
-            <p>Age: </p>
-            <p>Coat: </p>
-            <p>Color: </p>
-            <p>Location: </p>
-          </div>
-        </Grid>
-        <Grid item xs={12} className={classes.containerFour}>
-          <h3>About Me</h3>
-        </Grid>
-      </Grid>
+      </div>
     </div>
   );
 }
