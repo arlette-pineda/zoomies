7e3c4cf64428a0518aea8acbb90e3a922ce11527
Arlette Pineda <arlettepimo@gmail.com>
2021-03-09 01:56:49 +0000

    Added conditional to return null if thisDog object has not loaded.

--- client/components/dog-details.jsx
+++ client/components/dog-details.jsx
@@ -52,47 +52,56 @@ export default function DogDetails(props) {
   const classes = useStyles();
   const { dogId } = useParams();
   const [thisDog, setThisDog] = useState(null);
+  const [hasError, setErrors] = useState(false);
 
   const getDogId = () => {
     fetch(`/api/dogs/${dogId}`)
       .then(response => response.json())
       .then(res => {
         setThisDog(res);
-      });
+      })
+      .catch(() => setErrors(true));
   };
 
-  // getDogId();
+  useEffect(() => {
+    getDogId();
+  }, [dogId]);
 
-  return (
-    <div id="content-wrap">
-      <div className={classes.photosSection}>Photos</div>
-      <div className={classes.root} >
-        <Grid container >
-          <Grid container item xs={12} className={classes.titleSection}>
-            <Grid item xs={8} className={classes.nameSub}>
-              <h2>Dog Name {dogId} </h2>
-              <h3>Dog Breed</h3>
+  if (thisDog != null) {
+    return (
+      <div id="content-wrap">
+        <div className={classes.photosSection}>Photos</div>
+        <div className={classes.root} >
+          <Grid container >
+            <Grid container item xs={12} className={classes.titleSection}>
+              <Grid item xs={8} className={classes.nameSub}>
+                <h2>{thisDog.name}</h2>
+                <h3>{thisDog.breed.primary}</h3>
+              </Grid>
+              <Grid item xs={4} className={classes.scheduleArea}>
+                <h3>Schedule Meet!</h3>
+              </Grid>
             </Grid>
-            <Grid item xs={4} className={classes.scheduleArea}>
-              <h3>Schedule Meet!</h3>
+            <Grid item xs={12} className={classes.detailsSection}>
+              <h2>Details</h2>
+              <div className={classes.detailItems}>
+                <p>Size: {thisDog.size}</p>
+                <p>Gender: {thisDog.gender}</p>
+                <p>Age: {thisDog.age}</p>
+                <p>Coat: {thisDog.coat}</p>
+                <p>Color: {thisDog.color.primary}</p>
+                <p>Location: {thisDog.location.city},{thisDog.location.state}</p>
+              </div>
+            </Grid>
+            <Grid item xs={12} className={classes.aboutMeSection}>
+              <h2>About Me </h2>
+              <p>{thisDog.description}</p>
             </Grid>
           </Grid>
-          <Grid item xs={12} className={classes.detailsSection}>
-            <h2>Details</h2>
-            <div className={classes.detailItems}>
-              <p>Size: </p>
-              <p>Gender: </p>
-              <p>Age: </p>
-              <p>Coat: </p>
-              <p>Color: </p>
-              <p>Location: </p>
-            </div>
-          </Grid>
-          <Grid item xs={12} className={classes.aboutMeSection}>
-            <h2>About Me</h2>
-          </Grid>
-        </Grid>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (null);
+  }
 }
