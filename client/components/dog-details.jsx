c2863e36b2c551d1b232687eb9995d242b0f7e25
Arlette Pineda <arlettepimo@gmail.com>
2021-03-09 22:24:41 +0000

    CSS to match figma.

--- client/components/dog-details.jsx
+++ client/components/dog-details.jsx
@@ -20,22 +20,25 @@ const useStyles = makeStyles(theme => ({
     filter: 'drop-shadow(0 2mm 1mm lightgray)'
   },
   detailsSection: {
-    backgroundColor: '#ffcff5',
+    backgroundColor: 'white',
     height: '35vh',
     borderRadius: '25px 25px 0 0',
     paddingLeft: '30px'
   },
   aboutMeSection: {
-    backgroundColor: '#ffe1cf',
+    backgroundColor: 'white',
     height: '30vh',
     paddingLeft: '30px',
-    borderRadius: '0 0 25px 25px'
+    paddingRight: '30px',
+    borderRadius: '0 0 25px 25px',
+    filter: 'drop-shadow(0 2mm 0.3mm lightgray)'
   },
   nameSub: {
     backgroundColor: 'white',
     borderRadius: '25px 0 0 25px',
     paddingLeft: '30px',
-    textShadow: '0 6px 14px gray'
+    textShadow: '0 6px 14px #454545a1',
+    letterSpacing: '1px'
   },
   scheduleArea: {
     backgroundColor: theme.palette.secondary.main,
@@ -44,7 +47,27 @@ const useStyles = makeStyles(theme => ({
     paddingTop: '10px'
   },
   detailItems: {
-    filter: 'drop-shadow(0 2mm 1mm gray)'
+    textShadow: '0 6px 14px #454545a1',
+    color: 'black',
+    fontSize: '0.95rem'
+  },
+  breedsStyle: {
+    color: 'gray',
+    filter: 'none',
+    textShadow: 'none',
+    letterSpacing: 'normal'
+  },
+  secondaryBreedStyle: {
+    marginTop: '3px'
+  },
+  detailSpans: {
+    textShadow: 'none',
+    color: '#5b5959',
+    paddingLeft: '10px'
+  },
+  descriptionStyle: {
+    fontSize: '0.95rem',
+    color: '#5b5959'
   }
 }));
 
@@ -76,7 +99,9 @@ export default function DogDetails(props) {
             <Grid container item xs={12} className={classes.titleSection}>
               <Grid item xs={8} className={classes.nameSub}>
                 <h2>{thisDog.name}</h2>
-                <h3>{thisDog.breed.primary}</h3>
+                <h3 className={classes.breedsStyle}>{thisDog.breed.primary}
+                  {(thisDog.breed.secondary) ? <h5 className={classes.secondaryBreedStyle}> & {thisDog.breed.secondary}</h5>
+                    : null} </h3>
               </Grid>
               <Grid item xs={4} className={classes.scheduleArea}>
                 <h3>Schedule Meet!</h3>
@@ -85,17 +110,17 @@ export default function DogDetails(props) {
             <Grid item xs={12} className={classes.detailsSection}>
               <h2>Details</h2>
               <div className={classes.detailItems}>
-                <p>Size: {thisDog.size}</p>
-                <p>Gender: {thisDog.gender}</p>
-                <p>Age: {thisDog.age}</p>
-                <p>Coat: {thisDog.coat}</p>
-                <p>Color: {thisDog.color.primary}</p>
-                <p>Location: {thisDog.location.city},{thisDog.location.state}</p>
+                <p>Age: <span className={classes.detailSpans}>   {thisDog.age}</span></p>
+                <p>Size: <span className={classes.detailSpans}>   {thisDog.size}</span></p>
+                <p>Coat: <span className={classes.detailSpans}>  {thisDog.coat}</span></p>
+                <p>Color: <span className={classes.detailSpans}>   {thisDog.color.primary}</span></p>
+                <p>Gender: <span className={classes.detailSpans}>   {thisDog.gender}</span></p>
+                <p>Location: <span className={classes.detailSpans}>   {thisDog.location.city}, {thisDog.location.state}</span></p>
               </div>
             </Grid>
             <Grid item xs={12} className={classes.aboutMeSection}>
               <h2>About Me </h2>
-              <p>{thisDog.description}</p>
+              <p className={classes.descriptionStyle}>{thisDog.description}</p>
               <a href={thisDog.url} target="_blank" rel="noopener noreferrer">Adopt Me</a>
             </Grid>
           </Grid>
