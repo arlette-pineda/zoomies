3affc2cc4d85c6278ca186252776850d2c3a4f6b
Arlette Pineda <arlettepimo@gmail.com>
2021-05-11 08:00:53 +0000

    Added handleclick to component.

--- client/components/back-to-top.jsx
+++ client/components/back-to-top.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Zoom from '@material-ui/core/Zoom';
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    position: 'fixed'
-    // bottom: theme.spacing(2),
-    // right: theme.spacing(2)
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2)
   }
 }));
 
-export default function ScrollToTop(props) {
-  const { children, window } = props;
+export default function ScrollTop(props) {
+  const { children } = props;
   const classes = useStyles();
-
   const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
     disableHysteresis: true,
     threshold: 100
   });
@@ -37,3 +36,7 @@ export default function ScrollToTop(props) {
     </Zoom>
   );
 }
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired
+};
